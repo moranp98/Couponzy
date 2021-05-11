807e1ed38ec90e37c592dcd3215e85aa1142f489
saeedshibli <saeed.shibli.black@gmail.com>
2021-05-11 05:28:40 +0300

    signup with more fields

--- clientAngular/src/app/pages/roadstart-pages/sign-up/sign-up.component.ts
+++ clientAngular/src/app/pages/roadstart-pages/sign-up/sign-up.component.ts
@@ -13,7 +13,7 @@ export class PageSignUpComponent implements OnInit {
   pageTitle: string = 'רישום';
   isSignedIn = false
   public form: FormGroup;
-
+  public updateForm: FormGroup;
   constructor(private router: Router,
               public firebaseService : FirebaseService,
               public userService : UserService,
@@ -28,35 +28,66 @@ export class PageSignUpComponent implements OnInit {
     else
     this.isSignedIn = false
     this.form = this.fb.group({
-      email: [null, Validators.compose([Validators.required])]
-      // address: this.fb.group({ // make a nested group
-      //   city: [null, Validators.compose([Validators.required])],
-      //   street: [null, Validators.compose([Validators.required])],
-      //   country: [null, Validators.compose([Validators.required])]
-      // }),
-      // birthday:[null, Validators.compose([Validators.required])],
-      // gender:[null, Validators.compose([Validators.required])],
-      // maritalstatus:[null, Validators.compose([Validators.required])],
-      // userid:[null, Validators.compose([Validators.required])],
-      // username: this.fb.group({ // make a nested group
-      //   firstname: [null, Validators.compose([Validators.required])],
-      //   lastname: [null, Validators.compose([Validators.required])],
-      // }),
-      // phonenumber:[null, Validators.compose([Validators.required])],
+      email: [null, Validators.compose([Validators.required])],
+      address: this.fb.group({ // make a nested group
+        city: [null, Validators.compose([Validators.required])],
+        zipcode: [null, Validators.compose([Validators.required])],
+        country: [null, Validators.compose([Validators.required])]
+      }),
+      birthday:[null, Validators.compose([Validators.required])],
+      gender:[null, Validators.compose([Validators.required])],
+      maritalstatus:[null, Validators.compose([Validators.required])],
+      userid:[null, Validators.compose([Validators.required])],
+      username: this.fb.group({ // make a nested group
+        firstname: [null, Validators.compose([Validators.required])],
+        lastname: [null, Validators.compose([Validators.required])],
+      }),
+      phonenumber:[null, Validators.compose([Validators.required])],
     });
   }
   
-async onSignup(email:string,password:string,passwordVal:string){
+async onSignup(email:string,password:string,passwordVal:string,firstName:string,lastName:string,country:string,city:string,zipcode:string,birthday:string,maritalstatus:string,gender:string,phoneNumber:string,userid:string){
   if(password==passwordVal){
     await this.firebaseService.signup(email,password)
     if(this.firebaseService.isLoggedIn){
-      console.log(this.form.value)
-      this.userService.addUser(this.form.value).subscribe(
-        (user) => { console.log('Success', user); this.form.reset(); },
+      this.form=this.fb.group({
+        email:email,
+        city:city,
+        zipcode:zipcode,
+        country:country,
+        birthday:birthday,
+        gender:gender,
+        maritalstatus:maritalstatus,
+        userid:userid,
+        firstName:firstName,
+        lastName:lastName,
+        phoneNumber:phoneNumber
+      })
+      this.updateForm=this.fb.group({
+        email: [email, Validators.compose([Validators.required])],
+        address: this.fb.group({ // make a nested group
+          city: [city, Validators.compose([Validators.required])],
+          zipcode: [zipcode, Validators.compose([Validators.required])],
+          country: [country, Validators.compose([Validators.required])]
+        }),
+        birthday:[birthday, Validators.compose([Validators.required])],
+        gender:[gender, Validators.compose([Validators.required])],
+        maritalstatus:[maritalstatus, Validators.compose([Validators.required])],
+        userid:[userid, Validators.compose([Validators.required])],
+        username: this.fb.group({ // make a nested group
+          firstname: [firstName, Validators.compose([Validators.required])],
+          lastname: [lastName, Validators.compose([Validators.required])],
+        }),
+        phonenumber:[phoneNumber, Validators.compose([Validators.required])],
+      });
+
+      console.log(this.updateForm.value)
+      this.userService.addUser(this.updateForm.value).subscribe(
+        (user) => { console.log('Success', user); this.form.reset(); this.router.navigate(['/default-layout/dashboard']);},
         (error) => { console.log('Error', error); }
       );;
     this.isSignedIn = true
-    await this.router.navigate(['/default-layout/dashboard']);
+    //await this.router.navigate(['/default-layout/dashboard']);
     }
   }
 }
