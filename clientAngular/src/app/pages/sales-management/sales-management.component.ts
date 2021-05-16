e4db15490192682ca905a6ff925995d1ea10daf2
basheer mulla <Moranp98@gmail.com>
2021-05-16 15:46:12 +0300

    finish statistic page for shopManager

--- clientAngular/src/app/pages/sales-management/sales-management.component.ts
+++ clientAngular/src/app/pages/sales-management/sales-management.component.ts
@@ -2,11 +2,15 @@ import { Component, OnInit, ChangeDetectorRef, AfterViewInit } from '@angular/co
 import { SharedService } from '../../layouts/shared.service';
 import { ManageOrdersService } from 'src/app/services/manage-orders.service';
 import { ManageCouponsService } from '../../services/manage-coupons.service';
+import { ManageBranchesService } from '../../services/manage-branches.service';
 
 import { Router } from '@angular/router';
 import { Users } from '../../models/users';
 import { Orders } from '../../models/orders';
 import { Coupons } from '../../models/coupons';
+import { Branches } from '../../models/branches';
+
+import { ChartType } from 'chart.js';
 
 export var data = [];
 
@@ -22,54 +26,97 @@ export class PageSalesManagementComponent implements OnInit {
   orders: Orders[] = [];
   ordersListDisplay: any[] = [];
   coupons: Coupons[] = [];
+  branches: Branches[] = [];
 
-  resultCouponeName: any[];
-  resultCouponeNameToDisplayIntoTable: any[];
+  dataMapReduceChartsPieGrid: any[];
+  resultCouponName: any[];
+  amountCouponsRevenueToDisplayIntoTable: any[];
 
-  single: any[] = [];
+  dataMapReduceDoughnutChart: any[];
+  resultBranchName: any[];
+  amountBranchesRevenueToDisplayIntoTable: any[];
+  // <------- For ngx-charts-pie-grid ------->
   data: any[] = []
   view: any[] = [];
-
   // options
   showLegend: boolean = true;
   showLabels: boolean = true;
-
   colorScheme = {
     domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']
   };
+  ngAfterViewInit() {
+    this.cdr.detectChanges();
+  }
+  // events
+  onSelect(event) {
+    this.view = [innerWidth / 1.2];
+    console.log(event);
+  }
 
-  dataMapReduceChartsPieGrid: any[];
+  // <-------   For doughnut Chart   ------->
+  public doughnutChartLabels: string[] = [];
+  public doughnutChartData: number[] = [];
+
+  public donutColors=[
+    {
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)',
+        '#FFB1F6',
+        '#BCD0EB',
+        '#ADFFB0',
+        '#E8D292',
+        '#FFDBDB',
+        '#FAE6FF',
+        '#FCEDED',
+        '#E8A595',
+        '#EDA4D6',
+        '#FEE1CE',
+        '#7574FF',
+        '#14E3FF'
+    ]
+    }
+  ];
+  // <-------------- To get RandomColor: Not used yet -------------->
+  /*getRandomColor() {
+    var length = 6;
+    var chars = '0123456789ABCDEF';
+    var hex = '#';
+    while(length--) hex += chars[(Math.random() * 16) | 0];
+    return hex;
+  }*/
+
+  public doughnutChartType: ChartType = 'doughnut';
+  // events
+  public chartClicked(e:any):void {
+    this.view = [innerWidth / 1.2]; // use doughnut Chart to update the dimensions of the Pie-Grid chart
+    console.log(e);
+  }
 
   constructor(private _sharedService: SharedService,
     private _manageorders: ManageOrdersService,
     private ShowCouponsService: ManageCouponsService,
+    private ShowBranchesService: ManageBranchesService,
     private router: Router,
     private cdr: ChangeDetectorRef) {
-      this._sharedService.emitChange(this.pageTitle);
-      Object.assign(this, { data });
-      this.view = [innerWidth/1.2];
-    }
+    this._sharedService.emitChange(this.pageTitle);
+    Object.assign(this, { data });
+    this.view = [innerWidth / 1.2];
+  }
 
   ngOnInit(): void {
     var currentUser = localStorage.getItem('userDetails');
     this.currentUser = JSON.parse(currentUser)
-    
-    if (localStorage.getItem('user') == null || this.currentUser.role === 'seller') {
+
+    if (localStorage.getItem('user') == null || this.currentUser.role !== 'shopManager') {
       this.router.navigate(['/roadstart-layout/sign-in-social']);
     } else {
-      this.view = [innerWidth/1.2];
+      this.view = [innerWidth / 1.2];
       this.showCoupons(this.currentUser.employerId);
+      this.showBranches(this.currentUser.employerId);
       this.showOrders(this.currentUser.employerId);
     }
-  }
-
-  ngAfterViewInit() {
-    this.cdr.detectChanges();
-  }
-
-  onSelect(event) {
-    this.view = [innerWidth/1.2];
-    console.log(event);
   }
 
   showCoupons(shopId: string) {
@@ -81,91 +128,17 @@ export class PageSalesManagementComponent implements OnInit {
         });
         break;
       default: // role --> 'admin'
-        
+
         break;
     }
   }
-  
-  showOrders(shopId: string) {
+
+  showBranches(shopId: string) {
     switch (this.currentUser.role) {
       case 'shopManager':
-        this._manageorders.getAllOrders().subscribe((orders) => {
-          this.orders = orders.filter(order => order.branch['shopId'] === shopId);
-          console.log(this.orders);
-
-          this.ordersListDisplay = this.orders.map(order => {
-            return {...order, date: new Date(order.orderDate['_seconds'] * 1000)}
-          })
-
-          //For shopManager barChart
-          this.dataMapReduceChartsPieGrid = this.orders
-            .map(order => { return { branchName: order.coupon['couponName'], price: order.coupon['newPrice']}})
-            .reduce(function (obj, orderC, arr) {
-              if (!obj[orderC.branchName]) {
-                obj[orderC.branchName] = 1;
-              } else {
-                obj[orderC.branchName] += Number(orderC.price);
-              }
-              return obj;
-            }, []);
-          
-          const checkCouponNameReleventToShop = this.orders
-            .map(order => { return { name: order.coupon['couponName'] } })
-          this.resultCouponeName = Array.from(checkCouponNameReleventToShop
-            .reduce((m, t) => m.set(t.name, t), new Map())
-            .values());
-            console.log(this.resultCouponeName)
-            
-          this.data = this.resultCouponeName.map(coupon => { return {'name': coupon['name'], 'value': this.dataMapReduceChartsPieGrid[coupon['name']]} })
-          this.single = this.data
-
-          /*const couponNameTotalSale = this.coupons.map(coupon => {
-            return { couponName: coupon.couponName, total: 0 }
-          });*/
-          /*const couponNameTotalSale = this.coupons.map(coupon => {
-            return coupon.couponName
-          });
-          console.log(couponNameTotalSale)
-          const couponNameTotalSale2 = couponNameTotalSale.map(coupon => {
-            return { name: coupon, total: this.dataMapReduceChartsPieGrid[coupon] }
-          });
-          console.log(couponNameTotalSale2)
-
-          couponNameTotalSale.forEach(name => {
-            if (this.isCouponSale(name) > 0){
-
-            }
-          });
-          
-          console.log(couponNameTotalSale2)
-          /*couponNameTotalSale.forEach(coupon => {
-            coupon = this.dataMapReduceChartsPieGrid[coupon['coupon']]
-            //console.log(coupon['couponName'])
-          })*/
-          /*this.resultCouponeNameToDisplayIntoTable = couponNameTotalSale
-            .map(coupon => { return { couponName: coupon.couponName, total: coupon.total}})
-            .reduce(function (obj, orderC, arr) {
-              if (!obj[orderC.couponName]) {
-                obj[orderC.couponName] = 1;
-              } else {
-                obj[orderC.couponName] += Number(orderC.total);
-              }
-              return obj;
-            }, []);
-
-          console.log(this.resultCouponeNameToDisplayIntoTable)
-          console.log(this.resultCouponeName);*/
-          
-
-          /*this.resultCouponeNameToDisplayIntoTable.forEach(coupon => {
-            //coupon['total'] = this.dataMapReduceChartsPieGrid[coupon['couponName']];
-             if (this.resultCouponeName.find(c => c['name'] === coupon['couponName'])){
-              coupon['total'] = this.dataMapReduceChartsPieGrid[coupon['couponName']];
-            } else {
-              coupon['total'] = 0;
-            }
-          })
-          console.log(this.resultCouponeNameToDisplayIntoTable)*/
+        this.ShowBranchesService.getBranches().subscribe((branches) => {
+          this.branches = branches.filter(branch => (branch.isExists !== false && branch.shop.id === shopId));
+          console.log(this.branches)
         });
         break;
       default: // role --> 'admin'
@@ -174,11 +147,119 @@ export class PageSalesManagementComponent implements OnInit {
     }
   }
 
-  isCouponSale(couponName: string) {
-    if (!this.resultCouponeName.find(coupon => coupon['name'] === couponName)){
-      return 0;
-    } else {
-      return this.dataMapReduceChartsPieGrid[couponName];
+  showOrders(shopId: string) {
+    switch (this.currentUser.role) {
+      case 'shopManager':
+        this._manageorders.getAllOrders().subscribe((orders) => {
+          this.orders = orders.filter(order => order.branch['shopId'] === shopId);
+          console.log(this.orders);
+
+          this.ordersListDisplay = this.orders.map(order => {
+            return { ...order, date: new Date(order.orderDate['_seconds'] * 1000) }
+          })
+
+          // <-------------- For shopManager ngx-charts-pie-grid -------------->
+          this.dataMapReduceChartsPieGrid = this.orders
+            .map(order => { return { couponName: order.coupon['couponName'], price: order.coupon['newPrice'] } })
+            .reduce(function (obj, orderC, arr) {
+              if (!obj[orderC.couponName]) {
+                obj[orderC.couponName] = orderC.price;
+              } else {
+                obj[orderC.couponName] += orderC.price;
+              }
+              return obj;
+            }, []);
+
+          const checkCouponNameReleventToShop = this.orders
+            .map(order => { return { name: order.coupon['couponName'] } })
+
+          this.resultCouponName = Array.from(checkCouponNameReleventToShop
+            .reduce((m, t) => m.set(t.name, t), new Map())
+            .values());
+
+          this.data = this.resultCouponName.map(coupon => { return { 'name': coupon['name'], 'value': this.dataMapReduceChartsPieGrid[coupon['name']] } })
+
+          //For shopManager All Coupons Table
+          this.coupons.map(coupon => {
+            var isExists = this.resultCouponName.find(couponName => couponName['name'] === coupon.couponName)
+            if (!isExists) {
+              this.data.push({ 'name': coupon.couponName, 'value': 0 })
+              return this.resultCouponName.find(couponName => couponName['name'] === coupon.couponName)
+            } else {
+              return this.resultCouponName.find(couponName => couponName['name'] === coupon.couponName)
+            }
+          });
+
+          // Revenue amount from all Coupons
+          this.amountCouponsRevenueToDisplayIntoTable = this.data
+            .map(coupon => { return { couponName: coupon['name'], total: coupon['value'] } })
+            .reduce(function (obj, orderC, arr) {
+              if (!obj[orderC.couponName]) {
+                obj[orderC.couponName] = orderC.total;
+              } else {
+                obj[orderC.couponName] += orderC.total;
+              }
+              return obj;
+            }, []);
+          
+          // <-------------- For shopManager For doughnut Chart -------------->
+          this.dataMapReduceDoughnutChart = this.orders
+            .map(order => { return { branchName: order.branch['branchName'], price: order.coupon['newPrice'] } })
+            .reduce(function (obj, orderC, arr) {
+              if (!obj[orderC.branchName]) {
+                obj[orderC.branchName] = orderC.price;
+              } else {
+                obj[orderC.branchName] += orderC.price;
+              }
+              return obj;
+            }, []);
+          console.log(this.dataMapReduceDoughnutChart)
+          const checkBranchNameReleventToShop = this.orders
+            .map(order => { return { name: order.branch['branchName'] } })
+
+          this.resultBranchName = Array.from(checkBranchNameReleventToShop
+            .reduce((m, t) => m.set(t.name, t), new Map())
+            .values());
+          console.log(this.resultBranchName)
+          //this.data = this.resultCouponName.map(coupon => { return { 'name': coupon['name'], 'value': this.dataMapReduceChartsPieGrid[coupon['name']] } })
+            const dataDoughnutChart = this.resultBranchName.map(branch => { return { 'name': branch['name'], 'value': this.dataMapReduceDoughnutChart[branch['name']] } })
+          
+            //For shopManager All Branches Table
+          this.branches.map(branch => {
+            var isExists = this.resultBranchName.find(branchName => branchName['name'] === branch.branchName)
+            if (!isExists) {
+              dataDoughnutChart.push({ 'name': branch.branchName, 'value': 0 })
+              return this.resultBranchName.find(branchName => branchName['name'] === branch.branchName)
+            } else {
+              return this.resultBranchName.find(branchName => branchName['name'] === branch.branchName)
+            }
+          });
+          console.log(dataDoughnutChart)
+
+          dataDoughnutChart.forEach(branch => {
+            this.doughnutChartLabels.push(branch.name)
+            this.doughnutChartData.push(branch.value)
+          })
+          // Revenue amount from all Coupons
+          this.amountBranchesRevenueToDisplayIntoTable = dataDoughnutChart
+            .map(branch => { return { branchName: branch['name'], total: branch['value'] } })
+            .reduce(function (obj, orderC, arr) {
+              if (!obj[orderC.branchName]) {
+                obj[orderC.branchName] = orderC.total;
+              } else {
+                obj[orderC.branchName] += orderC.total;
+              }
+              return obj;
+            }, []);
+
+            console.log(this.amountBranchesRevenueToDisplayIntoTable)
+            console.log(this.doughnutChartLabels)
+            console.log(this.doughnutChartData)
+        });
+        break;
+      default: // role --> 'admin'
+
+        break;
     }
   }
 }
